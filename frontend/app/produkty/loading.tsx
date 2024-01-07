9284e786ec6e27755f5f87ed948852a44264afcc
Daniel Pátek <daniel.patek@email.cz>
2024-01-07 17:39:02 +0100

    fix: loading default value

--- frontend/app/produkty/loading.tsx
+++ frontend/app/produkty/loading.tsx
@@ -4,7 +4,7 @@ import FilterRow from "../../components/products/FilterRow";
 const Loading = () => {
     return (
         <main className="bg-base-100 text-base-content">
-            <FilterRow products={[]} />
+            <FilterRow products={[]} perPage={100} />
             <div className="flex w-full justify-center items-center">
                 <span className="loading loading-spinner loading-lg mt-5"></span>
             </div>
